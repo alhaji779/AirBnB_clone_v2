282107225790d79f4fdf31cf5535b4420b5e0af9
Michael Kanu <michaelokanu01@yahoo.com>
2024-04-16 08:58:49 -0700

    places-update

--- models/user.py
+++ models/user.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __table__ = "users"
+
+    __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship('Place',
+                          backref='user',
+                          cascade='all, delete-orphan')
