ea0eeceaef34d5db565dc0ce9b21be4e727cef8f
Michael Kanu <michaelokanu01@yahoo.com>
2024-04-17 14:46:29 -0700

    review-update

--- models/place.py
+++ models/place.py
@@ -2,20 +2,34 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Table, Integer, ForeignKey, Float
-
+from sqlalchemy.orm import relationship
+import os
+import models
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    __tablename__ = "places"
+    if os.environ.get('HBNB_TYPE_STORAGE') == 'db':    
+        __tablename__ = "places"
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+        reviews = relationship('Review', backref='place',
+                               cascade='all, delete-orphan',
+                               passive_deletes=True)
 
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
-    amenity_ids = []
+
+    if os.environ.get('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def reviews(self):
+            """ Review method for Filestorage
+            """
+            from models.review import Review
+            return [review for review in models.storage.all(Review).values()
+                    if review.place_id == self.id]
