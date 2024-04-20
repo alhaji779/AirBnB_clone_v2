ec4b0de2dc285f5dd6b45e3b044cce4c1b199b6e
Michael Kanu <michaelokanu01@yahoo.com>
2024-04-20 13:41:44 -0700

    code-debug2

--- models/amenity.py
+++ models/amenity.py
@@ -2,9 +2,18 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer
-
+from sqlalchemy.orm import relationship
+import os
 
 class Amenity(BaseModel, Base):
     """ Amenity Class"""
-    __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
+
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """ method to initialize Amenity """
+        super().__init__(*args, **kwargs)
--- models/place.py
+++ models/place.py
@@ -1,18 +1,86 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Table, Integer, ForeignKey, Float
+from sqlalchemy.orm import relationship
+import os
+import models
+from models.amenity import Amenity
 
 
-class Place(BaseModel):
+place_amenity = Table(
+    "place_amenity",
+    Base.metadata,
+    Column(
+        "place_id",
+        String(60),
+        ForeignKey("places.id"),
+        nullable=False),
+    Column(
+        "amenity_id",
+        String(60),
+        ForeignKey("amenities.id"),
+        nullable=False))
+
+
+class Place(BaseModel, Base):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+    if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "places"
+
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+        reviews = relationship('Review', backref='place',
+                               cascade='all, delete-orphan',
+                               passive_deletes=True)
+        amenities = relationship(
+                "Amenity", secondary=place_amenity,
+                backref="place_amenities", viewonly=False)
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
+    def __init__(self, *args, **kwargs):
+        """ method to initialise places """
+        super().__init__(*args, **kwargs)
+
+    if os.environ.get('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def reviews(self):
+            """ Review method for Filestorage
+            """
+            from models.review import Review
+            return [review for review in models.storage.all(Review).values()
+                    if review.place_id == self.id]
+
+        @property
+        def amenities(self):
+            """ Amenities method for FileStorage
+            """
+            return [amenity for amenity in models.storage.all(Amenity).values()
+                    if amenity.place_id == self.id]
+
+        @amenities.setter
+        def amenities(self, obj):
+            """ amenities setter
+            """
+            if not isinstance(obj, Amenity):
+                return
+            self.amenity_ids.append(obj.id)
